356d0e6cc14c7dde4bb78b60472e651941dad5c1
kasmil <kamilsalikhov75@gmail.com>
2023-08-14 23:44:48 +0300

    added effector, login and register pages

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,18 +1,18 @@
 import { useTranslation } from "react-i18next";
 import "./App.scss";
-import { Suspense, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
-import { Header } from "../components/Header/Header";
-import { Sidebar } from "../components/Sidebar/Sidebar";
-import { Flex } from "@chakra-ui/react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { useRoutes } from "../config/route/routes";
-import { ProfileSidebar } from "../components/ProfileSidebar/ProfileSidebar";
+import { $auth } from "../effector/auth";
+import { useStore } from "effector-react";
+import { DashboardLayout } from "../components/Layout/DashboardLayout";
+import { useEffect } from "react";
 
 const langs = ["ru", "en"];
 function App() {
+  const isAuth = useStore($auth);
   const { i18n } = useTranslation();
-  const { routes } = useRoutes();
-  
+  const { routes } = useRoutes({});
+
   useEffect(() => {
     if (!langs.includes(i18n.language)) {
       i18n.changeLanguage("ru");
@@ -20,30 +20,28 @@ function App() {
   }, [i18n]);
 
   return (
-    <Suspense fallback="">
-      <Flex width="100%">
-        <Sidebar />
-        <Flex
-          padding="25px"
-          w={{ base: "100%", xl: "calc(100% - 500px)" }}
-          flexDirection="column"
-          overflowY="auto"
-          height="100vh"
-        >
-          <Header />
-          <Routes>
-            {routes.map((route) => (
-              <Route
-                key={route.path}
-                path={route.path}
-                element={route.element}
-              />
-            ))}
-          </Routes>
-        </Flex>
-        <ProfileSidebar />
-      </Flex>
-    </Suspense>
+    <DashboardLayout>
+      <Routes>
+        {routes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={
+              isAuth &&
+              (route.path === "/login" || route.path === "/register") ? (
+                <Navigate to="/" />
+              ) : isAuth ||
+                route.path === "/login" ||
+                route.path === "/register" ? (
+                route.element
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+        ))}
+      </Routes>
+    </DashboardLayout>
   );
 }
 
